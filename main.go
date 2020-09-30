7b3ced3bc5c714af9b35bac20a59c7244a465d26
Alvaro Felix <alvaro.felix@alumnos.udg.mx>
2020-09-30 05:35:06 -0500

    Ciclos funcionando

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+//e = (1+1/n)^n
+
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var n float64
+	var e float64
+	var aux float64
+
+	fmt.Scanln(&n)
+
+	for i:= 1.0; i <= n; i++ {
+
+		e = (1+(1/i))
+		if i > 1 {
+			aux = e
+			for j:= 2.0; j <= i; j++{
+				e = e * aux 
+			}
+		}
+
+		//fmt.Println(e)
+	}
+	
 }
